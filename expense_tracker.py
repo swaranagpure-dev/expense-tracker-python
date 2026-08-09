a523fa2ab2e4b73997590a08e04c1e03563907c9
Swara Nagpure <swaran1040@gmail.com>
2026-08-09 16:57:26 +0530

    Add expense categories and dates

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,11 +1,13 @@
 import csv
-expenses=[]
+from datetime import date
+expenses = []
 
 def save_expenses():
     with open("expenses.csv", "w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=["name", "amount"])
+        writer = csv.DictWriter(file, fieldnames=["name", "amount","category", "date"])
         writer.writeheader()
         writer.writerows(expenses)
+
 def load_expenses():
     try:
         with open("expenses.csv", "r") as file:
@@ -13,25 +15,34 @@ def load_expenses():
             for row in reader:
                 expenses.append({
                     "name": row["name"],
-                    "amount": int(row["amount"])
+                    "amount": int(row["amount"]),
+                    "category": row["category"],
+                    "date": row["date"]
                 })
     except FileNotFoundError:
         pass
+
 def add_expenses():
         print("Add Expense Selected")
-        expense_name=input("Enter Expense Name:")
-        amount=int(input("Enter amount:"))
+        expense_name = input("Enter Expense Name:")
+        amount = int(input("Enter amount:"))
+        category = input("Enter category:")
+        expense_date = date.today().strftime("%d/%m/%Y")
+
         expense_data={"name":expense_name,
-        "amount":amount}
+        "amount":amount, "category": category,  "date": expense_date}
+
         expenses.append(expense_data)
         save_expenses()
+
 def view_expenses():
        
         if not expenses:
             print("No expenses found")
         else:
             for index, expense in enumerate(expenses, start=1):
-                print(f"{index}. {expense['name']}:{expense['amount']}")
+                print(f"{index}. {expense['name']} - ₹{expense['amount']} - {expense['category']} - {expense['date']}")
+
 def view_total_expenses():
         print("View Total Selected")
         total=0
@@ -39,6 +50,7 @@ def view_total_expenses():
             total+=expense['amount']
         print("====== Total Expense ======")
         print("Total Expense:", total)
+
 def delete_expenses():
         if not expenses:
             print("No expenses!")
@@ -49,11 +61,13 @@ def delete_expenses():
                 print("Invalid number")
             else:
                 deleted=expenses.pop(delete_expense - 1)
+                save_expenses()
                 print("Expense Deleted Successfully!" )
                 print("Deleted Expenses:")
-                print(f"{deleted['name']}:{deleted['amount']}")
+                print(f"{deleted['name']} - ₹{deleted['amount']} - {deleted['category']} - {deleted['date']}")
                 print("Remaining expenses:")
                 view_expenses()
+
 load_expenses()
 while True:
     print("======Expense Tracker======\n")
